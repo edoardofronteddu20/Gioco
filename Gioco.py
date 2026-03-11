c4f66f22c63251d7d1e112025976576367ecdd00
edoardofronteddu20 <edoardo.fronteddu@itiszuccante.edu.it>
2026-03-11 10:55:47 +0100

    aa

--- Gioco.py
+++ Gioco.py
@@ -1,139 +1,89 @@
 import arcade
+import arcade.future.background as background
 
-GRAVITA = 0.5
-VELOCITA_SALTO = 11
-VELOCITA_NORMALE = 5
-VELOCITA_SPRINT = 9 
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 800
+CAMERA_SPEED = 0.1
+PLAYER_SPEED = 300
 
-class Gioco(arcade.Window):
-    def __init__(self, larghezza, altezza, titolo):
-        super().__init__(larghezza, altezza, titolo)
 
-        self.sfondo_list = arcade.SpriteList()
-        self.lista_sprite = arcade.SpriteList()
-        self.pavimento_list = arcade.SpriteList()
-        self.piattaforme_superiori = arcade.SpriteList()
+class GameView(arcade.View):
 
-        self.sprite = None
-        self.physics_engine = None
-        self.scala = 0.15
-        
-    
-        self.texture_idle = None
-        self.texture_run = None
+    def __init__(self):
+        super().__init__()
 
-        self.left_pressed = False
-        self.right_pressed = False
-        self.shift_pressed = False
-        self.salti_effettuati = 0
+        self.background_color = (162, 84, 162, 255)
 
-        self.setup()
+        # camera
+        self.camera = arcade.Camera2D()
 
-    def setup(self):
-        # sfondo
-        sfondo = arcade.Sprite("./assets/background.png")
-        sfondo.center_x = self.width / 2
-        sfondo.center_y = self.height / 2
-        sfondo.width = self.width
-        sfondo.height = self.height
-        self.sfondo_list.append(sfondo)
+        # gruppo parallax
+        self.backgrounds = background.ParallaxGroup()
 
-        # png
-        self.texture_idle = arcade.load_texture("./assets/sprite.png")
-        self.texture_run = arcade.load_texture("./assets/run.png")
+        bg_size = (WINDOW_WIDTH, WINDOW_HEIGHT)
 
-        # sprite
-        self.sprite = arcade.Sprite()
-        self.sprite.texture = self.texture_idle
-        self.sprite.scale = self.scala
-        self.sprite.center_x = 100 
-        self.sprite.center_y = 450 
-        self.lista_sprite.append(self.sprite)
+        # layer sfondo
+        self.backgrounds.add_from_file("assets/layers/cielo.png", size=bg_size, depth=10.0)
+        self.backgrounds.add_from_file("assets/layers/montagne.png", size=bg_size, depth=5.0)
+        self.backgrounds.add_from_file("assets/layers/alberi.png", size=bg_size, depth=3.0)
+        self.backgrounds.add_from_file("assets/layers/strada.png", size=bg_size, depth=1.0)
 
-        # "pavimento"
-        pavimento = arcade.SpriteSolidColor(1000, 40, arcade.color.TRANSPARENT_BLACK)
-        pavimento.center_x = 500
-        pavimento.center_y = 40 
-        self.pavimento_list.append(pavimento)
+        # player
+        self.player = arcade.Sprite("assets/player.png")
+        self.player.center_x = 0
+        self.player.bottom = 0
 
-        # "pavimento 2"
-        coords_piani = [
-            [350, 190, 700, 20], # Secondo piano
-            [170, 345, 340, 20], # Terzo Sinistra
-            [830, 345, 340, 20], # Terzo Destra
-        ]
-
-        for c in coords_piani:
-            p = arcade.SpriteSolidColor(int(c[2]), int(c[3]), arcade.color.TRANSPARENT_BLACK)
-            p.center_x = c[0]
-            p.center_y = c[1]
-            self.piattaforme_superiori.append(p)
-
-        self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.sprite,
-            platforms=self.piattaforme_superiori,
-            walls=self.pavimento_list,
-            gravity_constant=GRAVITA
-        )
+        self.x_velocity = 0
 
     def on_draw(self):
         self.clear()
-        self.sfondo_list.draw()
-        self.lista_sprite.draw()
+        self.camera.use()
 
-    def on_update(self, delta_time):
-        if self.physics_engine.can_jump():
-            self.salti_effettuati = 0
+        bg = self.backgrounds
 
-        # shift
-        velocita_attuale = VELOCITA_NORMALE
-        
-        if self.shift_pressed and (self.left_pressed or self.right_pressed):
-            velocita_attuale = VELOCITA_SPRINT
-            self.sprite.texture = self.texture_run
-        else:
-            self.sprite.texture = self.texture_idle
+        bg.offset = self.camera.bottom_left
+        bg.pos = self.camera.bottom_left
 
-        # Movimento
-        self.sprite.change_x = 0
-        if self.left_pressed and not self.right_pressed:
-            self.sprite.change_x = -velocita_attuale
-            self.sprite.scale = (-self.scala, self.scala)
-        elif self.right_pressed and not self.left_pressed:
-            self.sprite.change_x = velocita_attuale
-            self.sprite.scale = (self.scala, self.scala)
+        bg.draw()
 
-        # Limiti bordi
-        if self.sprite.left < 0: self.sprite.left = 0
-        if self.sprite.right > self.width: self.sprite.right = self.width
+        self.player.draw()
 
-        self.physics_engine.update()
+    def pan_camera_to_player(self):
 
-    def on_key_press(self, tasto, modificatori):
-        if tasto == arcade.key.W or tasto == arcade.key.SPACE:
-            if self.salti_effettuati < 2:
-                self.sprite.change_y = VELOCITA_SALTO
-                self.salti_effettuati += 1
-        elif tasto == arcade.key.S:
-            self.sprite.center_y -= 5
-        elif tasto == arcade.key.A:
-            self.left_pressed = True
-        elif tasto == arcade.key.D:
-            self.right_pressed = True
-        elif tasto == arcade.key.LSHIFT or tasto == arcade.key.RSHIFT:
-            self.shift_pressed = True
+        self.camera.position = arcade.math.lerp_2d(
+            self.camera.position,
+            (self.player.center_x, WINDOW_HEIGHT // 2),
+            CAMERA_SPEED
+        )
 
-    def on_key_release(self, tasto, modificatori):
-        if tasto == arcade.key.A:
-            self.left_pressed = False
-        elif tasto == arcade.key.D:
-            self.right_pressed = False
-        elif tasto == arcade.key.LSHIFT or tasto == arcade.key.RSHIFT:
-            self.shift_pressed = False
+    def on_update(self, delta_time: float):
+
+        # movimento player
+        self.player.center_x += self.x_velocity * delta_time
+
+        # camera segue
+        self.pan_camera_to_player()
+
+    def on_key_press(self, symbol: int, modifiers: int):
+
+        if symbol == arcade.key.A:
+            self.x_velocity = -PLAYER_SPEED
+
+        elif symbol == arcade.key.D:
+            self.x_velocity = PLAYER_SPEED
+
+    def on_key_release(self, symbol: int, modifiers: int):
+
+        if symbol == arcade.key.A or symbol == arcade.key.D:
+            self.x_velocity = 0
+
 
 def main():
-    Gioco(1000, 600, "Gioco")
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, "Parallax Example")
+    view = GameView()
+    window.show_view(view)
     arcade.run()
+
 
 if __name__ == "__main__":
     main()
